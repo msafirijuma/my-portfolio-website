121cd0dad1e7e43f950a40c0782bc1335c2a088a
Msafiri Juma Esmail <msafirijesmail@gmail.com>
2023-01-25 13:14:04 +0300

    add projects to project section

--- scripts/homepage-themes.js
+++ scripts/homepage-themes.js
@@ -206,7 +206,7 @@ if (darkMode == "enabled") {
 
 // Dark AND Light Toggler
 themeToggler.addEventListener("click", () => {
-    window.location.reload();
+    // window.location.reload();
     darkMode = localStorage.getItem("dark-mode");
 
     if (darkMode === "disabled") {
